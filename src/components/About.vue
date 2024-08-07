5691fe96f5bfac9fff621606981d97bd4ed06824
EUI-CHEOL <68554956+EUI-CHEOL@users.noreply.github.com>
2024-08-07 23:30:42 +0900

    Add files via upload
    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about-text">
+    <h2>About Weather App</h2>
+    <p>Developer: Ironofchoi</p>
+    <p>Last update: 2024.8</p>
+    <p>Email: ironofchoi@gmail.com</p>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import store from './store/store.js';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+//사용할 아이콘 불러오기
+import { faBarsStaggered, faLocationDot, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+
+// 아이콘 등록하기
+library.add(faBarsStaggered, faLocationDot, faMagnifyingGlass);
+
+createApp(App)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
